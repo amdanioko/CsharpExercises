861dacc5a943099b24ad1b7cc8b9e9c966f5ef4c
Aminata Danioko <37707013+amdanioko@users.noreply.github.com>
2019-07-22 14:47:24 -0400

    Add files via upload

---
+++ TextAnalyzers.Lib/WordSearcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAnalyzers.Lib
+{
+    public class WordSearcher
+
+    {
+        public static WordSearcher FromFile(string filePath)
+        {
+            return new WordSearcher(File.ReadAllText(filePath));
+        }
+        public WordSearcher(string document)
+        {
+            if (string.IsNullOrEmpty(document))
+                throw new ArgumentNullException(nameof(document)); 
+            Document = document;
+        }
+
+ 
+        public string Document { get; private set; }
+
+        public int GetWordCount(string searchTerm, bool isCaseSensitive = true)
+        {
+            StringComparison scomp = isCaseSensitive ? StringComparison.InvariantCulture :
+                StringComparison.InvariantCultureIgnoreCase;
+            int count = 0;
+            int ndx = Document.IndexOf(searchTerm, scomp);
+            while (ndx >= 0)
+            {
+                count++;
+                ndx = Document.IndexOf(searchTerm, ndx + searchTerm.Length, scomp);
+            }
+            return count;
+        }
+            
+
+      
+    }
+}
+  
